434c23a200925f501cf8def6aeb25de4abc0af7c
One2ThreeZJM <253521624@qq.com>
2017-03-01 10:04:45 +0800

    Update HTMLTestRunner.py

--- common/HTMLTestRunner.py
+++ common/HTMLTestRunner.py
@@ -1,6 +1,6 @@
 """
 A TestRunner for use with the Python unit testing framework. It
-generates a HTML report to show the result at a glance.
+generates a HTML report to show the result at a glance.11
 
 The simplest way to use this is to invoke its main method. E.g.
 
